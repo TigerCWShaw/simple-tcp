57d65041e28314a91624e4bc4478b2a643d84a4d
Tiger Shaw <113390740+TigerCWShaw@users.noreply.github.com>
2024-05-01 21:49:44 -0400

    Delete newudp dir

--- newudpl-1.7/host2ip.h
+++
@@ -1,9 +0,0 @@
-/*
- * host2ip.h - return IP address given a host name
- *
- * Copyright 1998-2001 by Columbia University; all rights reserved 
-*/
-
-#include "sysdep.h"
-
-extern struct in_addr host2ip(char *host);
